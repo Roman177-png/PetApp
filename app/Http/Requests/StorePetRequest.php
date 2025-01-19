a82c0ed058ceb493100021792e849b752c682457
Roman177-png <miron-2002@ukr.net>
2025-01-19 23:54:26 +0100

    add getPetById, show, updatePet, deletePet method

---
+++ app/Http/Requests/StorePetRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'status' => 'required|in:available,pending,sold',
+            'photoUrls' => 'required|array',
+            'tags' => 'nullable|array',
+            'category_id' => 'nullable|integer',
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\Controller::class,'index']);
+Route::resource('pets', \App\Http\Controllers\PetController::class);
