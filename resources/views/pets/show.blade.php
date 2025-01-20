0d2f97a07ab3934e0c3994c4b373b6c9de36c258
Roman177-png <miron-2002@ukr.net>
2025-01-20 03:10:34 +0100

    Handle tag display in pet details view

--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -6,12 +6,25 @@
     <div class="bg-white p-6 rounded-lg shadow-md">
         <h2 class="text-2xl font-bold mb-4">{{ $pet['name'] }}</h2>
         <p>Status: <span class="text-blue-600">{{ $pet['status'] }}</span></p>
+
         <p>Photo URLs:</p>
         <ul>
             @foreach ($pet['photoUrls'] as $url)
                 <li><a href="{{ $url }}" target="_blank" class="text-blue-600 hover:underline">{{ $url }}</a></li>
             @endforeach
         </ul>
+
+        @if (!empty($pet['tags']) && count($pet['tags']) > 0)
+            <p class="mt-4">Tags:</p>
+            <ul>
+                @foreach ($pet['tags'] as $tag)
+                    <li class="text-gray-700">- {{ $tag['name'] }}</li>
+                @endforeach
+            </ul>
+        @else
+            <p class="mt-4 text-gray-500">No tags available.</p>
+        @endif
+
         <a href="{{ route('pets.index') }}" class="text-blue-600 hover:underline mt-4 block">Back to List</a>
     </div>
 @endsection
